6519360227805b5db7b501b9c9ae66ad7580b517
Guilherme Gomes da Silva <guinusguiu@gmail.com>
2026-06-24 11:52:49 -0300

    init comit

---
+++ mybar-spring/src/test/java/br/ufpi/mybar_spring/MybarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpi.mybar_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
