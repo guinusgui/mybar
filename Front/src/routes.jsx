b0f0b530c8d6ca0f384c9c99ae15e5d832cb681e
Lince <licesgg@gmail.com>
2026-07-05 14:17:29 -0300

    [STYLE] Aplique prettier em arquivos com erros de estilo

--- Front/src/routes.jsx
+++ Front/src/routes.jsx
@@ -6,40 +6,40 @@ import SearchAccounts from "./components/Screens/SearchAccounts";
 import UserManagingScreen from "./components/Screens/UserManagingScreen";
 
 const routes = [
-    {
-        label: "Registro de Contas",
-        route: "/gerenciar/contas",
-        element: <SearchAccounts/>
-    },
-        {
-        label: "Gestão de Itens do Cardápio",
-        route: "/gerenciar/itensMenu",
-        element: <MenuManagingScreen/>
-    },
-        {
-        label: "Gestão de Tipos de Itens",
-        route: "/gerenciar/tiposItens",
-        element: <ItemTypeManagingScreen/>
-    },
-        {
-        label: "Controle da Cozinha",
-        route: "/gerenciar/cozinha",
-        element: <KitchenControlScreen/>
-    },
-    {
-        label: "Controle de Entregas",
-        route: "/gerenciar/cozinha",
-        element: <DeliveryControlScreen/>
-    },
-    {
-        label: "Configuração do Sistema",
-        route: "/gerenciar/configuracoes",
-        element: <DeliveryControlScreen/>
-    },
-    {
-        label: "Gestão de Usuários",
-        route: "/gerenciar/usuario",
-        element: <UserManagingScreen/>
-    },
-]
+  {
+    label: "Registro de Contas",
+    route: "/gerenciar/contas",
+    element: <SearchAccounts />,
+  },
+  {
+    label: "Gestão de Itens do Cardápio",
+    route: "/gerenciar/itensMenu",
+    element: <MenuManagingScreen />,
+  },
+  {
+    label: "Gestão de Tipos de Itens",
+    route: "/gerenciar/tiposItens",
+    element: <ItemTypeManagingScreen />,
+  },
+  {
+    label: "Controle da Cozinha",
+    route: "/gerenciar/cozinha",
+    element: <KitchenControlScreen />,
+  },
+  {
+    label: "Controle de Entregas",
+    route: "/gerenciar/cozinha",
+    element: <DeliveryControlScreen />,
+  },
+  {
+    label: "Configuração do Sistema",
+    route: "/gerenciar/configuracoes",
+    element: <DeliveryControlScreen />,
+  },
+  {
+    label: "Gestão de Usuários",
+    route: "/gerenciar/usuario",
+    element: <UserManagingScreen />,
+  },
+];
 export default routes;
