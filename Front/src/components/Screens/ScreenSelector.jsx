a6abe56f77abb2f1b08cb012321e0270369874e9
Lince <licesgg@gmail.com>
2026-07-05 14:14:07 -0300

    [FIX] Remova strngs desnecessárias no `ScreenSelector.jsx`

--- Front/src/components/Screens/ScreenSelector.jsx
+++ Front/src/components/Screens/ScreenSelector.jsx
@@ -1,20 +1,27 @@
-import DeliveryControlScreen from "./DeliveryControlScreen"
-import ItemTypeManagingScreen from "./ItemTypeManagingScreen"
-import KitchenControlScreen from "./KitchenControlScreen"
-import MenuManagingScreen from "./MenuManagingScreen"
-import SearchAccounts from "./SearchAccounts"
-import UserManagingScreen from "./UserManagingScreen"
+import DeliveryControlScreen from "./DeliveryControlScreen";
+import ItemTypeManagingScreen from "./ItemTypeManagingScreen";
+import KitchenControlScreen from "./KitchenControlScreen";
+import MenuManagingScreen from "./MenuManagingScreen";
+import SearchAccounts from "./SearchAccounts";
+import UserManagingScreen from "./UserManagingScreen";
 
-import routes from "@/routes"
-import { Link } from "react-router-dom"
+import routes from "@/routes";
+import { Link } from "react-router-dom";
 
 export default function ScreenSelector() {
-    return (
-<div className="border border-gray-400 rounded-xl grid grid-cols-2 gap-2 p-4 gap-x-8 gap-y-4">
-    {routes.map((endpoint) => {
-        return <Link className="juicyButton border p-2 rounded-xl text-center" key={endpoint.label} to={endpoint.route}> {endpoint.label} </Link>
-
-    })}
-</div> 
-    )
+  return (
+    <div className="border border-gray-400 rounded-xl grid grid-cols-2 gap-2 p-4 gap-x-8 gap-y-4">
+      {routes.map((endpoint) => {
+        return (
+          <Link
+            className="juicyButton border p-2 rounded-xl text-center"
+            key={endpoint.label}
+            to={endpoint.route}
+          >
+            {endpoint.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
